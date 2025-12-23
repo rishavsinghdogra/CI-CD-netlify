59582b79f6bb2e0f5161ed31cef2a7da28a2a98e
Rishav Singh Dogra <rishavsd2000@gmail.com>
2025-12-23 11:32:01 +0530

    fix: update message in read-the-docs paragraph to reflect deployment check

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        checking if branch merge in dev triggers a deploy
       </p>
     </>
   )
